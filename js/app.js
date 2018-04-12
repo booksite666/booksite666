d309fe77a1d2a3fc0d21dff3453a9e681c02e93c
Rakesh Sagar <rakeshsagar64@gmail.com>
2018-04-12 14:06:00 +0530

    mad eit static i think

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app=angular.module("myApp",[]);
     app.controller("myController",function($scope,$http){
 
       $scope.title="myAngular app";
-      $http.get("https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=2a6c01f9753c469cb31a08e46e1d1ff4").then(function(entries){
+      $http.get("data/data.json").then(function(entries){
         $scope.stuffs=entries.data.results;
         console.log(entries.data);
       });
